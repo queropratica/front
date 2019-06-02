ad90288eb2b540a8d4fe5e0044d6566eb1dcbe71
Rafael Delbel <rafael.bernardes@gsw.com.br>
2019-06-02 03:33:53 -0300

    Correction in mentor and student views

--- src/app/components/main-card/maincard.directive.js
+++ src/app/components/main-card/maincard.directive.js
@@ -11,7 +11,8 @@
             restrict: 'E',
             templateUrl: 'app/components/main-card/maincard.html',
             scope: {
-                creationDate: '='
+                creationDate: '=',
+                item: '='
             },
             controller: MainCardController,
             controllerAs: 'vm',
@@ -22,30 +23,6 @@
 
         /** @ngInject */
         function MainCardController() {
-            var vm = this;
-
-            vm.slides = [
-                {
-                    title: "1 title",
-                    image: 'http://lorempixel.com/560/400/sports/1',
-                },
-                {
-                    title: "2 title",
-                    image: 'http://lorempixel.com/560/400/sports/2',
-                },
-                {
-                    title: "3 title",
-                    image: 'http://lorempixel.com/560/400/sports/3',
-                },
-                {
-                    title: "4 title",
-                    image: 'http://lorempixel.com/560/400/sports/4',
-                },
-                {
-                    title: "5 title",
-                    image: 'http://lorempixel.com/560/400/sports/5',
-                },
-            ];
         }
     }
 
--- src/app/mentor/mentor.controller.js
+++ src/app/mentor/mentor.controller.js
@@ -15,14 +15,11 @@
             disqus_url: 'https://queropratica.github.io'
         };
 
-        vm.subjects = [
-            {'_id': 1, 'name': 'Lean'},
-            {'_id': 2, 'name': '4 Ano'},
-            {'_id': 3, 'name': 'Processos'},
-            {'_id': 4, 'name': 'Lean'},
-            {'_id': 5, 'name': '4 Ano'},
-            {'_id': 6, 'name': 'Processos'},
-            {'_id': 7, 'name': 'Lean'}
+        vm.projects = [
+            {title: 'Alohomora', summary: 'Alohomora do Agreste', owner: 'Owner PO', publishedAt: '25/05/2019'},
+            {title: 'Alohomora', summary: 'Alohomora do Campo', owner: 'Owner Project', publishedAt: '26/05/2019'},
+            {title: 'Alohomora', summary: 'Alohomora do Alom', owner: 'Owner Tes', publishedAt: '21/05/2019'},
+            {title: 'Alohomora', summary: 'Alohomora do Adalon', owner: 'Owner Fas', publishedAt: '29/05/2019'}
         ]
 
         projectFactory.load()
