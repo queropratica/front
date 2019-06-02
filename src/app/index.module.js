91768982f197a1d0ca9f4d29c8458d6c31908505
Paulo Luan <pauloluan.inova@gmail.com>
2019-06-02 00:30:44 -0300

    Update 2019-06-02T03:30:41.717Z

--- src/app/index.module.js
+++
@@ -1,7 +0,0 @@
-(function() {
-    'use strict';
-
-    angular
-        .module('angularSeedApp', ['ngAnimate', 'ngCookies', 'ngTouch',
-            'ngSanitize', 'ngResource', 'ui.router']);
-})();
