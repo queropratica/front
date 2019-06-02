64bea1b0c8f105d8ecaae7098e4589f4a7e7b6f8
Guto Oliveira <luizaugusto.contato@gmail.com>
2019-06-01 22:49:28 -0300

    adicionando sandbox na nova estrutura

---
+++ gutao-sandbox/config.rb
@@ -0,0 +1,15 @@
+# Require any additional compass plugins here.
+
+# Paths configuration
+css_dir = "css"
+sass_dir = "sass"
+
+# Prefere relative paths in the CSS
+relative_assets = true
+
+# Display the original location (SCSS file and line) of the selector
+# Useful for debugging
+line_comments = true
+
+# SCSS and not SASS
+preferred_syntax = :scss
