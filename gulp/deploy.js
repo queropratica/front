a37f6bc6c84934154a45a5aa6a7ed2e712c3ab65
Paulo Luan <pauloluan.inova@gmail.com>
2019-06-02 09:21:02 -0300

    updated favicon

--- gulp/deploy.js
+++ gulp/deploy.js
@@ -1,11 +1,12 @@
-'use strict';
+'use strict'
 
-var gulp = require('gulp');
-var $ = require('gulp-load-plugins')();
+var gulp = require('gulp')
+var $ = require('gulp-load-plugins')()
 
-gulp.task('deploy', function() {
-    return gulp.src('./dist/**/*')
+gulp.task('deploy', function () {
+  return gulp.src('./dist/**/*')
         .pipe($.ghPages({
-            force: true
-        }));
-});
+          branch: 'master',
+          force: true
+        }))
+})
