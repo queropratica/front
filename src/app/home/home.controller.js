0ca008b7a3c302752b8d64a20c6cc1a606c00a74
Rafael Delbel <rafael.bernardes@gsw.com.br>
2019-06-02 05:25:21 -0300

    Added page of home

---
+++ src/app/home/home.controller.js
@@ -0,0 +1,30 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('angularSeedApp')
+        .controller('HomeController', HomeController);
+
+    /** @ngInject */
+    function HomeController(projectFactory, $scope) {
+        var vm = this;
+
+        $scope.disqusConfig = {
+            disqus_shortname: 'https-queropratica-github-io',
+            disqus_identifier: 'mentor_screen_',
+            disqus_url: 'https://queropratica.github.io'
+        };
+
+        vm.projects = [
+            {title: 'Alohomora', summary: 'Alohomora do Agreste', owner: 'Owner PO', publishedAt: '25/05/2019'},
+            {title: 'Alohomora', summary: 'Alohomora do Campo', owner: 'Owner Project', publishedAt: '26/05/2019'},
+            {title: 'Alohomora', summary: 'Alohomora do Alom', owner: 'Owner Tes', publishedAt: '21/05/2019'},
+            {title: 'Alohomora', summary: 'Alohomora do Adalon', owner: 'Owner Fas', publishedAt: '29/05/2019'}
+        ]
+
+        projectFactory.load()
+            .$promise.then(function (projects) {
+                vm.subjects = projects
+            })
+    }
+})();
